a25a9c0f4d7a392f1526c8b54c03d1283e4beae5
PROB8 <blackb8r@gmail.com>
2018-03-12 17:19:35 -0400

    update to /server/index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const runServer = require('./server').runServer;
 
 if (process.env.NODE_ENV === 'production') {
-  runServer(undefined, process.env.PORT || 8080);
+  runServer(process.env.PORT || 8080);
 }
 else {
   const app = express();
