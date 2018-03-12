ab797d31c2986747a4ec1da8410e7a6e70cc3ff6
PROB8 <blackb8r@gmail.com>
2018-03-12 18:03:37 -0400

    updates to scss and AOS is now working

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import aos from 'aos'
+import AOS from 'aos'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div class="backgrounda">
-            <h1>BACKGROUND 1</h1>
-            <div class='shadow'>
+    constructor(props, context) { 
+        super(props, context); 
+        AOS.init({
+            easing: 'ease-in-out-sine'
+          }); 
+    } 
+   
+
+    render() {
+
+        return (
+        <div className="App">
+            <div className="backgrounda">
+                <h1>BACKGROUND 1</h1>
+                <div className='shadow'>
+                </div>
+            </div>
+            <div className='this-div'>
+                PEEP DIS SHIT
+            </div>
+            <div className="backgroundb">
+                <h1>BACKGROUND 2</h1>
+                <div className='shadow'>
+                </div>
+            </div>
+            <div className='this-div'>
+            <div className='box-blue' data-aos="fade-right">
+                look at me
+            </div>
+            <div className='box-red' data-aos="fade-left">
+                look at me
+            </div>
+            </div>
+            <div className="backgroundc">
+                <h1>BACKGROUND 3</h1>
+                <div className='shadow'>
+            </div>
+            </div>
+            <div className='this-div'>
+           
             </div>
         </div>
-        <div class='this-div'>
-            PEEP DIS SHIT
-        </div>
-        <div class="backgroundb">
-            <h1>BACKGROUND 2</h1>
-            <div class='shadow'>
-            </div>
-        </div>
-        <div class='this-div'>
-        </div>
-        <div class="backgroundc">
-            <h1>BACKGROUND 3</h1>
-            <div class='shadow'>
-        </div>
-        </div>
-        <div class='this-div'>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
