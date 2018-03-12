6e7d2971d21467dc98df0a75dea8cd0726a72368
PROB8 <blackb8r@gmail.com>
2018-03-12 17:06:10 -0400

    update to /server/index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express       = require('express');
 const app           = express()
 const path          = require('path')
 
-
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req,res) => {
     const index = path.resolve(__dirname, '../client/build', 'index.html');
