41caeed84499c8eb09e739c715b454670b77b2e6
PROB8 <blackb8r@gmail.com>
2018-03-16 21:09:52 -0400

    update

--- client/src/gtngPartner.js
+++ client/src/gtngPartner.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import './gp.css';
 
 export default class GTNGPartner extends Component {
-
+    componentDidMount() {
+    }
 
     render() {
         
@@ -10,9 +11,12 @@ export default class GTNGPartner extends Component {
             <div id='modal' style={{display:this.props.display}}>
                 
                 <h1  className='close'onClick={this.props.modalDisplay} > x </h1>
-                <iframe 
-                    allowfullscreen='true'
-                    allowtransparency='true'
+                <iframe
+                    id='iframe' 
+                    allowFullScreen='true'
+                    allowTransparency='true'
+                    marginheight='80px'
+                    marginwidth='80px'
                     src="https://form.jotform.com/80746079884169">
                 </iframe>
             </div>
