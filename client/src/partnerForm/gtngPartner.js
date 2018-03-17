39afe33de7cdb8783e1c92ef461c85d8986697e1
PROB8 <blackb8r@gmail.com>
2018-03-16 23:00:34 -0400

    update

--- client/src/partnerForm/gtngPartner.js
+++ client/src/partnerForm/gtngPartner.js
@@ -9,12 +9,13 @@ export default class GTNGPartner extends Component {
         
         return (
             <div id='modal' style={{display:this.props.display}}>
-               
+                {/* <h1  className='close'onClick={this.props.modalDisplay} > close </h1> */}
                 <div className='wrapper'>
-                    <h1  className='close'onClick={this.props.modalDisplay} > close </h1>
+                   
                     <iframe
-                        id='iframe' 
-                        scrolling='yes'
+                        height="315" width="560" 
+                        allowfullscreen="" 
+                        frameborder="0"     
                         src="https://form.jotform.com/80746079884169">
                     </iframe>
                 </div>                
