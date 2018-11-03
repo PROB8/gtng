1b2d2fbf57e4e579568712eafc31f3ce557ea34d
Jaha Naeem Gitonga <blackb8r@gmail.com>
2018-11-03 00:56:30 -0400

    chore(sections): header/section 1 created

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import AOS from 'aos';
 import Footer from './footer/footer';
-import StickyNav from './stickyNav/stickyNav';
-import SlideShow from './slidshow/slideshow';
+import SectionOne from './sections/section-one';
 import ReturnArrow from './rtnArrow/returnArrow';
 import ProjectStack from './project-stack';
 
@@ -30,33 +29,7 @@ class App extends Component {
         return (
             <div className="App">
                 <ReturnArrow />
-                <header className="backgrounda">
-                    <div className="shadow-1">
-                        <div className="header-wrap">
-                            <h1 className="header">GTNG™</h1>
-                            <p className="slogan">
-                                <em>boundless</em>
-                            </p>
-                        </div>
-                    </div>
-                </header>
-                <StickyNav />
-                <div className="this-div3 first">
-                    <SlideShow />
-                    <div className="content-1">
-                        <p>
-                            Welcome! Here at GTNG we pride ourselves on being
-                            customer oriented and delivering on our promises.
-                            While we build your app, you can expect to be well
-                            informed and taken care of throughout the entire
-                            process. We want you to know that you are in great
-                            hands. Trust us to be confidential and protect your
-                            intellectual property as if it were our own and
-                            let's get started bringing your app or website to
-                            life today!
-                        </p>
-                    </div>
-                </div>
+                <SectionOne />
                 <div className="backgroundb">
                     <div className="shadow" />
                 </div>
