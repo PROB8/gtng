63b823e18043192298ea70da96ef9e6af255a58c
Jaha Naeem Gitonga <blackb8r@gmail.com>
2018-11-06 00:17:15 -0500

    chore: make section components and speparate functions

---
+++ client/src/sections/index.js
@@ -0,0 +1,14 @@
+import Head from './head';
+import SectionOne from './section-one';
+import SectionTwo from './section-two';
+import { ShadowSectionOne, ShadowSectionTwo } from './shadows';
+import SectionThree from './section-three';
+
+export {
+    Head,
+    SectionOne,
+    SectionTwo,
+    SectionThree,
+    ShadowSectionOne,
+    ShadowSectionTwo,
+}
